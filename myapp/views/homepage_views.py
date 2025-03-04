43cec9e3d1c1ca7993ce5d43cce1cd78ce99431c
Andrei Yorku <andreip@yorku.com>
2025-03-03 22:59:44 -0500

    --

--- myapp/views/homepage_views.py
+++ myapp/views/homepage_views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from ..forms import HTMLInputForm
 from .utils import get_generated_chapters
-#test
+#testyteeee
 def html_input_view(request):
     form = HTMLInputForm(request.POST or None)
     chapters = get_generated_chapters()
