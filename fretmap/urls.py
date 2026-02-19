fce6b843a98fa93e6e6dc95feb8d29a3ccf2e56a
andreiyorku <andreip@my.yorku.ca>
2026-02-19 14:07:30 -0500

    fretmap

--- fretmap/urls.py
+++ fretmap/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = 'fretmap'
+
 urlpatterns = [
-    path('', views.index, name='fretmap'),
+    path('', views.index, name='index'),
     path('get_user_data/', views.get_user_data, name='get_user_data'),
     path('save/', views.save_transition, name='save_transition'),
+
+    # NEW: Add this line to handle the settings slider saves
+    path('save_settings/', views.save_settings, name='save_settings'),
 ]
