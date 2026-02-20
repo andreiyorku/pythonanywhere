37894a4fc307615b9d03c6664a0fc77425cff153
andreiyorku <andreip@my.yorku.ca>
2026-02-19 20:04:01 -0500

    fretmap

--- fretmap/urls.py
+++ fretmap/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'fretmap'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='fretmap'),
     path('get_user_data/', views.get_user_data, name='get_user_data'),
     path('save_transition/', views.save_transition, name='save_transition'),
     path('save_settings/', views.save_settings, name='save_settings'),
